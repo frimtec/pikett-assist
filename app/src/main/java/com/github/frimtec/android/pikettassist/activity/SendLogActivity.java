b4440b051327734ca39c32e14e0345291a4fdf38
Markus Friedli <frimtec@gmx.ch>
2019-10-08 14:28:01 +0200

    #54 Code cleanup

--- app/src/main/java/com/github/frimtec/android/pikettassist/activity/SendLogActivity.java
+++ app/src/main/java/com/github/frimtec/android/pikettassist/activity/SendLogActivity.java
@@ -13,7 +13,7 @@ import static android.content.Intent.EXTRA_BUG_REPORT;
 
 public class SendLogActivity extends AppCompatActivity {
 
-  public final static String ACTION_SEND_LOG = "android.permission.SEND_LOG";
+  public static final String ACTION_SEND_LOG = "android.permission.SEND_LOG";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {

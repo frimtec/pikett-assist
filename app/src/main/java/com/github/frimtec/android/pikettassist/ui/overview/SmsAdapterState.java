a5db4db8911c1ab922621f5f6405daeb28e53c19
Markus Friedli <frimtec@gmx.ch>
2022-04-29 10:38:10 +0200

    #311 Remove permission REQUEST_INSTALL_PACKAGES (#312)

--- app/src/main/java/com/github/frimtec/android/pikettassist/ui/overview/SmsAdapterState.java
+++ app/src/main/java/com/github/frimtec/android/pikettassist/ui/overview/SmsAdapterState.java
@@ -1,18 +1,16 @@
 package com.github.frimtec.android.pikettassist.ui.overview;
 
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 import static com.github.frimtec.android.pikettassist.ui.overview.State.TrafficLight.GREEN;
 import static com.github.frimtec.android.pikettassist.ui.overview.State.TrafficLight.RED;
 import static com.github.frimtec.android.pikettassist.ui.overview.State.TrafficLight.YELLOW;
 import static com.github.frimtec.android.securesmsproxyapi.SecureSmsProxyFacade.S2MSP_PACKAGE_NAME;
 
-import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.Settings;
 import android.text.Html;
 import android.text.SpannableString;
 import android.text.TextUtils;
@@ -101,67 +99,42 @@ class SmsAdapterState extends State {
 
   private void openDownloadDialog(Context context, @StringRes int message, @StringRes int title, Installation installation) {
     if (isFDroidAvailable(context)) {
-      SpannableString htmlMessage = new SpannableString(Html.fromHtml(context.getString(message) + "<br><br>" + context.getString(R.string.install_from_fdroid), Html.FROM_HTML_MODE_COMPACT));
+      SpannableString htmlMessage = new SpannableString(Html.fromHtml(context.getString(message) + "<br><br>" + context.getString(R.string.install_with_fdroid), Html.FROM_HTML_MODE_COMPACT));
       AlertDialog alertDialog = new FDroidSmsAdapterInstallDialog(context, htmlMessage, title);
       alertDialog.show();
       enableLinks(alertDialog);
     } else {
-      if (!context.getPackageManager().canRequestPackageInstalls()) {
-        SpannableString htmlMessage = new SpannableString(Html.fromHtml(context.getString(message) + "<br><br>" + context.getString(R.string.permission_sms_text_unknown_source_request), Html.FROM_HTML_MODE_COMPACT));
-        AlertDialog alertDialog = new AlertDialog.Builder(context)
-            // set dialog message
-            .setTitle(title)
-            .setMessage(htmlMessage)
-            .setCancelable(true)
-            .setPositiveButton(stateContext.getString(R.string.s2msp_settings), (dialog, which) ->
-                stateContext.getContext().startActivity(new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES, Uri.parse("package:com.github.frimtec.android.pikettassist"))))
-            .setNegativeButton(R.string.general_cancel, (dialog, which) -> {
-            }).create();
-        alertDialog.show();
-        enableLinks(alertDialog);
-      } else {
-        SpannableString htmlMessage = new SpannableString(Html.fromHtml(stateContext.getString(R.string.s2msp_download_request), Html.FROM_HTML_MODE_COMPACT));
-        AlertDialog alertDialog = new AlertDialog.Builder(context)
-            // set dialog message
-            .setTitle(title)
-            .setMessage(htmlMessage)
-            .setCancelable(true)
-            .setPositiveButton(R.string.general_download, (dialog, which) -> {
-              DownloadManager.Request request = new DownloadManager.Request(installation.getDownloadLink());
-              request.setTitle("S2MSP version " + installation.getApiVersion());
-              request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-              request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, String.format("s2msp-app-%s.apk", installation.getApiVersion()));
-              DownloadManager manager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
-              if (manager != null) {
-                manager.enqueue(request);
-                Toast.makeText(context, R.string.s2msp_download_started, Toast.LENGTH_LONG).show();
-              }
-            })
-            .setNegativeButton(R.string.general_cancel, (dialog, which) -> {
-            }).create();
-        alertDialog.show();
-        enableLinks(alertDialog);
-      }
+      // let the browser handle the stuff
+      SpannableString htmlMessage = new SpannableString(Html.fromHtml(context.getString(message) + context.getString(R.string.install_with_browser), Html.FROM_HTML_MODE_COMPACT));
+      AlertDialog alertDialog = new AlertDialog.Builder(context)
+          // set dialog message
+          .setTitle(title)
+          .setMessage(htmlMessage)
+          .setCancelable(true)
+          .setPositiveButton(R.string.general_download, (dialog, which) -> {
+            Intent openBrowserIntent = new Intent(Intent.ACTION_VIEW, installation.getDownloadLink());
+            openBrowserIntent.addFlags(FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(openBrowserIntent);
+          })
+          .setNegativeButton(R.string.general_cancel, (dialog, which) -> {
+          }).create();
+      alertDialog.show();
+      enableLinks(alertDialog);
     }
   }
 
   private static class FDroidSmsAdapterInstallDialog extends AlertDialog {
+
     public FDroidSmsAdapterInstallDialog(Context context, SpannableString htmlMessage, @StringRes int title) {
       super(context);
       setTitle(title);
       setMessage(htmlMessage);
       setCancelable(true);
-      setButton(AlertDialog.BUTTON_POSITIVE, getContext().getString(R.string.general_install), (dialog, which) -> context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.github.frimtec.android.securesmsproxy"))));
-      setButton(AlertDialog.BUTTON_NEUTRAL, getContext().getString(R.string.add_repo), (dialog, which) -> {
-        // this will never be called
+      setButton(AlertDialog.BUTTON_POSITIVE, getContext().getString(R.string.general_install), (dialog, which) -> context.startActivity(
+          Intent.createChooser(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.github.frimtec.android.securesmsproxy")), "Select F-Droid"))
+      );
+      setButton(AlertDialog.BUTTON_NEGATIVE, getContext().getString(R.string.general_cancel), (dialog, which) -> {
       });
-      setButton(AlertDialog.BUTTON_NEGATIVE, getContext().getString(R.string.general_cancel), (dialog, which) -> {});
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      getButton(AlertDialog.BUTTON_NEUTRAL).setOnClickListener(v -> getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://apt.izzysoft.de/fdroid/repo?fingerprint=3BF0D6ABFEAE2F401707B6D966BE743BF0EEE49C2561B9BA39073711F628937A"))));
     }
   }
 

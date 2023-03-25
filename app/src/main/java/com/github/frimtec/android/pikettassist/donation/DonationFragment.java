39257a94958685a4f1052e653eef5440fcaad8e7
Markus Friedli <frimtec@gmx.ch>
2023-03-25 16:26:25 +0100

    Add alternative donation possibility for installation form alternative stores (#415)

--- app/src/main/java/com/github/frimtec/android/pikettassist/donation/DonationFragment.java
+++ app/src/main/java/com/github/frimtec/android/pikettassist/donation/DonationFragment.java
@@ -1,8 +1,12 @@
 package com.github.frimtec.android.pikettassist.donation;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +15,7 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -35,6 +40,34 @@ public class DonationFragment extends DialogFragment {
   private ArticleAdapter adapter;
   private TextView errorTextView;
   private BillingProvider billingProvider;
+  private boolean noBillingService = false;
+
+  private static final String PAYPAL_ME_BASE_LINK = "https://paypal.me/frimtec";
+  private static final String PAYPAL_ME_LINK = PAYPAL_ME_BASE_LINK + "?country.x=CH&locale.x=de_DE";
+  private static final String ALTERNATIVE_DONATION_TEXT_TEMPLATE = "" +
+      "<h3>" +
+      "%s" +
+      "</h3>" +
+      "</p>" +
+      "<p>" +
+      "<br>" +
+      "<a href=\"" + PAYPAL_ME_LINK + "\">" +
+      "<img src=\"paypalme.png\"/>" +
+      "</<a>" +
+      "<br>" +
+      "<br>" +
+      "<h1>" +
+      "<a href=\"" + PAYPAL_ME_LINK + "\">" +
+      PAYPAL_ME_BASE_LINK +
+      "</a>" +
+      "</h1>" +
+      "</p>" +
+      "<p>" +
+      "<br>" +
+      "<h3>" +
+      "%s" +
+      "</h3>" +
+      "</p>";
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,14 +77,41 @@ public class DonationFragment extends DialogFragment {
 
   @Nullable
   @Override
-  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                           @Nullable Bundle savedInstanceState) {
-    View root = inflater.inflate(R.layout.acquire_fragment, container, false);
-    errorTextView = root.findViewById(R.id.error_textview);
-    recyclerView = root.findViewById(R.id.list);
-    if (billingProvider != null) {
+  public View onCreateView(
+      LayoutInflater inflater,
+      @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState
+  ) {
+    View root = inflater.inflate(R.layout.donation_fragment, container, false);
+    this.errorTextView = root.findViewById(R.id.error_textview);
+    this.recyclerView = root.findViewById(R.id.list);
+
+    if (this.noBillingService) {
+      TextView alternativeDonationText = root.findViewById(R.id.alternative_donation_text);
+      alternativeDonationText.setVisibility(View.VISIBLE);
+      alternativeDonationText.setText(Html.fromHtml(
+          String.format(
+              ALTERNATIVE_DONATION_TEXT_TEMPLATE,
+              getString(R.string.alternative_donation_text),
+              getString(R.string.alternative_donation_thanks)
+          ),
+          Html.FROM_HTML_MODE_COMPACT,
+          source -> {
+            Drawable image = ResourcesCompat.getDrawable(getResources(), R.drawable.paypalme, null);
+            if (image != null) {
+              image.setBounds(0, 0, 550, 215);
+            }
+            return image;
+          },
+          null
+      ));
+      alternativeDonationText.setMovementMethod(LinkMovementMethod.getInstance());
+      this.recyclerView.setVisibility(View.GONE);
+      this.errorTextView.setVisibility(View.GONE);
+    } else if (this.billingProvider != null) {
       handleManagerAndUiReady();
     }
+
     // Setup a toolbar for this fragment
     Toolbar toolbar = root.findViewById(R.id.toolbar);
     toolbar.setNavigationIcon(R.drawable.ic_arrow_up);
@@ -60,9 +120,10 @@ public class DonationFragment extends DialogFragment {
     return root;
   }
 
+  @SuppressLint("NotifyDataSetChanged")
   public void refreshUI() {
-    if (adapter != null) {
-      adapter.notifyDataSetChanged();
+    if (this.adapter != null) {
+      this.adapter.notifyDataSetChanged();
     }
   }
 
@@ -70,10 +131,15 @@ public class DonationFragment extends DialogFragment {
    * Notifies the fragment that billing manager is ready and provides a BillingProviders
    * instance to access it
    */
-  public void onManagerReady(BillingProvider billingProvider) {
-    this.billingProvider = billingProvider;
-    if (recyclerView != null) {
-      handleManagerAndUiReady();
+  public void onManagerReady(@Nullable BillingProvider billingProvider) {
+    if (billingProvider != null) {
+      this.billingProvider = billingProvider;
+      this.noBillingService = false;
+      if (this.recyclerView != null) {
+        handleManagerAndUiReady();
+      }
+    } else {
+      this.noBillingService = true;
     }
   }
 
@@ -85,29 +151,29 @@ public class DonationFragment extends DialogFragment {
     if (getActivity() == null || getActivity().isFinishing()) {
       return;
     }
-    errorTextView.setVisibility(View.VISIBLE);
+    this.errorTextView.setVisibility(View.VISIBLE);
     int billingResponseCode = billingProvider.getBillingManager().getBillingClientResponseCode();
 
     if (billingResponseCode == BillingClient.BillingResponseCode.BILLING_UNAVAILABLE) {
-      errorTextView.setText(getText(R.string.error_billing_unavailable));
+      this.errorTextView.setText(getText(R.string.error_billing_unavailable));
     } else {
-      errorTextView.setText(getText(R.string.error_billing_default));
+      this.errorTextView.setText(getText(R.string.error_billing_default));
     }
   }
 
   private void queryProductDetails() {
     if (getActivity() != null && !getActivity().isFinishing()) {
       final List<ProductRowData> dataList = new ArrayList<>();
-      adapter = new ArticleAdapter();
-      UiManager uiManager = createUiManager(adapter, billingProvider);
-      adapter.setUiManager(uiManager);
+      this.adapter = new ArticleAdapter();
+      UiManager uiManager = createUiManager(this.adapter, this.billingProvider);
+      this.adapter.setUiManager(uiManager);
       // Once we added all the subscription items, fill the in-app items rows below
       addProductRows(dataList, uiManager.getDelegatesFactory().getProductList());
     }
   }
 
   private void addProductRows(List<ProductRowData> inList, List<Product> products) {
-    billingProvider.getBillingManager().querySkuDetailsAsync(products,
+    this.billingProvider.getBillingManager().querySkuDetailsAsync(products,
         (billingResult, productDetails) -> {
           FragmentActivity activity = getActivity();
           if (activity != null) {
@@ -125,16 +191,16 @@ public class DonationFragment extends DialogFragment {
                 if (inList.size() == 1) {
                   displayAnErrorIfNeeded();
                 } else {
-                  if (recyclerView.getAdapter() == null) {
-                    recyclerView.setAdapter(adapter);
+                  if (this.recyclerView.getAdapter() == null) {
+                    this.recyclerView.setAdapter(this.adapter);
                     Context context = getContext();
                     if (context != null) {
                       Resources res = getContext().getResources();
-                      recyclerView.addItemDecoration(new CardsWithHeadersDecoration(adapter, (int) res.getDimension(R.dimen.header_gap), (int) res.getDimension(R.dimen.row_gap)));
+                      this.recyclerView.addItemDecoration(new CardsWithHeadersDecoration(this.adapter, (int) res.getDimension(R.dimen.header_gap), (int) res.getDimension(R.dimen.row_gap)));
                     }
-                    recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                    this.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
                   }
-                  adapter.updateData(inList);
+                  this.adapter.updateData(inList);
                 }
               } else {
                 displayAnErrorIfNeeded();

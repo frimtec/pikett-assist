324c70153625faac44ff67d6368339f63b45d21d
Markus Friedli <frimtec@gmx.ch>
2023-12-15 21:55:50 +0100

    Workaround ongoing notifications that can be closed by user on android 14 (#485)

--- app/src/main/java/com/github/frimtec/android/pikettassist/service/NotificationActionListener.java
+++ app/src/main/java/com/github/frimtec/android/pikettassist/service/NotificationActionListener.java
@@ -1,13 +1,19 @@
 package com.github.frimtec.android.pikettassist.service;
 
+import static com.github.frimtec.android.pikettassist.service.system.NotificationService.ACTION_CLOSE_ALARM;
+import static com.github.frimtec.android.pikettassist.service.system.NotificationService.ACTION_LOW_BATTERY_NOTIFICATION_CLOSED_BY_USER;
+import static com.github.frimtec.android.pikettassist.service.system.NotificationService.ACTION_ON_CALL_NOTIFICATION_CLOSED_BY_USER;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
 import com.github.frimtec.android.pikettassist.action.Action;
-
-import static com.github.frimtec.android.pikettassist.service.system.NotificationService.ACTION_CLOSE_ALARM;
+import com.github.frimtec.android.pikettassist.domain.BatteryStatus;
+import com.github.frimtec.android.pikettassist.service.system.BatteryService;
+import com.github.frimtec.android.pikettassist.service.system.NotificationService;
+import com.github.frimtec.android.pikettassist.state.ApplicationPreferences;
 
 public class NotificationActionListener extends BroadcastReceiver {
 
@@ -17,11 +23,19 @@ public class NotificationActionListener extends BroadcastReceiver {
   public void onReceive(Context context, Intent intent) {
     String action = intent.getAction();
     if (action != null) {
-      if (ACTION_CLOSE_ALARM.equals(action)) {
-        new AlertService(context).closeAlert();
-        context.sendBroadcast(new Intent(Action.REFRESH.getId()));
-      } else {
-        Log.e(TAG, "Unknown action: " + action);
+      switch (action) {
+        case ACTION_CLOSE_ALARM -> {
+          new AlertService(context).closeAlert();
+          context.sendBroadcast(new Intent(Action.REFRESH.getId()));
+        }
+        case ACTION_ON_CALL_NOTIFICATION_CLOSED_BY_USER -> PikettWorker.enqueueWork(context);
+        case ACTION_LOW_BATTERY_NOTIFICATION_CLOSED_BY_USER -> {
+          BatteryStatus batteryStatus = new BatteryService(context).batteryStatus();
+          if (batteryStatus.level() <= ApplicationPreferences.instance().getBatteryWarnLevel(context)) {
+            new NotificationService(context).notifyBatteryLow(batteryStatus);
+          }
+        }
+        default -> Log.e(TAG, "Unknown action: " + action);
       }
     }
   }
